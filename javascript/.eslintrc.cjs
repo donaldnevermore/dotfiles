7fe86e5d15134eee94e05592e9aeda018e4e8b52
Donald33 Wang <donald33wang@gmail.com>
2022-12-16 22:04:06 +0800

    Update js

--- javascript/.eslintrc.cjs
+++ javascript/.eslintrc.cjs
@@ -1,30 +1,39 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true,
         "node": true
     },
-    "extends": ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 2021,
+        "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "plugins": ["react", "@typescript-eslint"],
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
     "rules": {
-        "indent": "warn",
+        indent: "warn",
         "brace-style": ["warn", "stroustrup"],
         "linebreak-style": "warn",
-        "quotes": "warn",
+        quotes: "warn",
         "space-before-function-paren": [
             "warn",
             {
-                "anonymous": "always",
-                "named": "never",
-                "asyncArrow": "always"
+                anonymous: "always",
+                named: "never",
+                asyncArrow: "always"
             }
         ],
         "arrow-spacing": "warn",
@@ -34,11 +43,12 @@
         "space-infix-ops": "warn",
         "@typescript-eslint/space-infix-ops": "warn",
         "space-unary-ops": "warn",
-        "semi": ["warn", "never"],
+        semi: ["warn", "never"],
         "semi-spacing": "warn",
         "space-before-blocks": "warn",
         "@typescript-eslint/type-annotation-spacing": "warn",
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-non-null-assertion": "off"
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "no-constant-condition": "off"
     }
 }
