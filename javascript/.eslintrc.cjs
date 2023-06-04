388a2d9521104130727b5d3be1fce6660091c90a
Donald33 Wang <donald33wang@gmail.com>
2023-06-04 17:31:59 +0800

    Update

--- javascript/.eslintrc.cjs
+++ javascript/.eslintrc.cjs
@@ -1,48 +1,65 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        indent: ["warn", 4, { SwitchCase: 0 }],
-        semi: ["warn", "never"],
-        quotes: ["warn", "double"],
-        "linebreak-style": ["warn", "unix"],
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "linebreak-style": ["warn", "unix"],
+    "arrow-spacing": "warn",
+    "space-unary-ops": "warn",
+    "semi-spacing": "warn",
+    "arrow-parens": "warn",
+    "switch-colon-spacing": "warn",
+    "no-multi-spaces": "warn",
+    "space-in-parens": "warn",
 
-        "space-before-function-paren": [
-            "warn",
-            {
-                anonymous: "always",
-                named: "never",
-                asyncArrow: "always"
-            }
-        ],
-        "arrow-spacing": "warn",
-        "comma-spacing": "warn",
-        "keyword-spacing": "warn",
-        "object-curly-spacing": ["warn", "always"],
-        "space-infix-ops": "warn",
-        "@typescript-eslint/space-infix-ops": "warn",
-        "space-unary-ops": "warn",
-        "semi-spacing": "warn",
-        "space-before-blocks": "warn",
-        "@typescript-eslint/type-annotation-spacing": "warn",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-non-null-assertion": "off"
-    }
-}
+    // TypeScript rules.
+    "brace-style": "off",
+    "@typescript-eslint/brace-style": ["warn", "1tbs"],
+    indent: "off",
+    "@typescript-eslint/indent": ["warn", 2, { SwitchCase: 1 }],
+    "quotes": "off",
+    "@typescript-eslint/quotes": ["warn", "double"],
+    semi: "off",
+    "@typescript-eslint/semi": ["warn", "always"],
+
+    "object-curly-spacing": "off",
+    "@typescript-eslint/object-curly-spacing": ["warn", "always"],
+    "keyword-spacing": "off",
+    "@typescript-eslint/keyword-spacing": "warn",
+    "space-before-function-paren": "off",
+    "@typescript-eslint/space-before-function-paren": [
+      "warn",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always"
+      }
+    ],
+    "comma-spacing": "off",
+    "@typescript-eslint/comma-spacing": "warn",
+    "space-before-blocks": "off",
+    "@typescript-eslint/space-before-blocks": "warn",
+    "key-spacing": "off",
+    "@typescript-eslint/key-spacing": "warn",
+    "space-infix-ops": "off",
+    "@typescript-eslint/space-infix-ops": "warn",
+
+    "@typescript-eslint/type-annotation-spacing": "warn",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off"
+  }
+};
--- javascript/.prettierrc.cjs
+++ javascript/.prettierrc.cjs
@@ -1,7 +1,4 @@
 module.exports = {
-    bracketSameLine: true,
-    printWidth: 120,
-    trailingComma: "none",
-    tabWidth: 4,
-    semi: false
-}
+  bracketSameLine: true,
+  printWidth: 120,
+};
